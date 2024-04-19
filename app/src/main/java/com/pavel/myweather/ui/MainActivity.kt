a8d90f5f45b96832544cd3eb4efec90211de3692
Alive <7116499@gmail.com>
2024-04-19 12:28:24 +0300

    ui

--- app/src/main/java/com/pavel/myweather/ui/MainActivity.kt
+++ app/src/main/java/com/pavel/myweather/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.pavel.myweather
+package com.pavel.myweather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pavel.myweather.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //findNavController(R.id.nav_graf).navigate(R.id.action_global_weatherForDayFragment)
     }
+
 }
